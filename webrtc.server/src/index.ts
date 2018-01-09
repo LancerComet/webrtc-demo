635f9c13fdeaf4df2fb117bd7746d88187b2b98f
LancerComet <chw644@hotmail.com>
2018-01-09 23:07:41 +0800

    + [WIP]

--- webrtc.server/src/index.ts
+++ webrtc.server/src/index.ts
@@ -3,23 +3,37 @@ import * as IO from 'socket.io'
 
 const HOST = '0.0.0.0'
 const PORT = 3000
+const isDev = process.env.NODE_ENV === 'development'
 
+// 创建 Http 服务器来响应 WebSocket 握手.
 const httpServer = http.createServer((req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080')
-	res.setHeader('Access-Control-Request-Method', '*')
-	res.setHeader('Access-Control-Allow-Methods', '*')
-  res.setHeader('Access-Control-Allow-Headers', '*')
-  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  // 在开发环境下允许跨域请求，方便调试.
+  if (isDev) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080')
+    res.setHeader('Access-Control-Request-Method', '*')
+    res.setHeader('Access-Control-Allow-Methods', '*')
+    res.setHeader('Access-Control-Allow-Headers', '*')
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+  }
 
+  // 对于外部 Http 请求，返回一个简易的数据.
   res.writeHead(200, {
-    'Content-Type': 'text/html'
+    'Content-Type': 'application/json'
   })
 
-  res.end('<h1>It works!</h1>')
+  res.end(JSON.stringify({
+    status: 200,
+    message: 'OK'
+  }))
 })
 
+// 创建 WebSocket 服务器，用于充当 Signaling 服务器.
 const io = IO(httpServer)
-io.origins('*:*')
+
+// 在开发环境下允许跨域请求，方便调试.
+if (isDev) {
+  io.origins('*:*')
+}
 
 io.on('connection', function (socket) {
   console.log('A new user is on.')
