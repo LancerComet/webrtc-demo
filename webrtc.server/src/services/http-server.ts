9ebfe98dff83aead438bcd2f77705cd02d7405c4
LancerComet <chw644@hotmail.com>
2018-01-10 11:28:25 +0800

    + [WIP]

---
+++ webrtc.server/src/services/http-server.ts
@@ -0,0 +1,37 @@
+import * as http from 'http'
+
+/**
+ * 创建 http 服务器.
+ *
+ * @param {any} { corsOrigin = '' }
+ * @returns {http.Server}
+ */
+function createHttpServer ({ corsOrigin = '' }): http.Server {
+  // 创建 Http 服务器来响应 WebSocket 握手.
+  const httpServer = http.createServer((req, res) => {
+    // 在开发环境下允许跨域请求，方便调试.
+    if (corsOrigin) {
+      res.setHeader('Access-Control-Allow-Origin', corsOrigin)
+      res.setHeader('Access-Control-Request-Method', '*')
+      res.setHeader('Access-Control-Allow-Methods', '*')
+      res.setHeader('Access-Control-Allow-Headers', '*')
+      res.setHeader('Access-Control-Allow-Credentials', 'true')
+    }
+
+    // 对于外部 Http 请求，返回一个简易的数据.
+    res.writeHead(200, {
+      'Content-Type': 'application/json'
+    })
+
+    res.end(JSON.stringify({
+      status: 200,
+      message: 'OK'
+    }))
+  })
+
+  return httpServer
+}
+
+export {
+  createHttpServer
+}
---
+++ webrtc.server/src/services/signaling-server.ts
@@ -0,0 +1,45 @@
+import * as http from 'http'
+import * as IO from 'socket.io'
+
+/**
+ * 创建 Signaling 服务器.
+ *
+ * @param {ICreateSignalingServer} option
+ * @returns {SocketIO.Server}
+ */
+function createSignalingServer (option: ICreateSignalingServer): SocketIO.Server {
+  if (!option.httpServer) {
+    console.error('[Error] Please provide a http server instance while creating signaling server.')
+    return null
+  }
+
+  const io = IO(option.httpServer)
+
+  if (option.corsRule) {
+    io.origins(option.corsRule)
+  }
+
+  return io
+}
+
+/**
+ * Signaling 服务器业务初始化函数.
+ *
+ * @param {SocketIO.Server} io
+ */
+function initSignalingServer (io: SocketIO.Server) {
+  io.on('connection', function (socket) {
+    console.log('A new user is on.')
+    socket.emit('greeting', 'Hello!')
+  })
+}
+
+export {
+  createSignalingServer,
+  initSignalingServer
+}
+
+interface ICreateSignalingServer {
+  httpServer: http.Server
+  corsRule: string
+}
